e92560ecd3fde67e85075788da071e20008f8437
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-11 12:23:57 +0300

    Completing the first task.

---
+++ HomeWork_3 _1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Найти кубы чисел от 1 до N.
+
+int Readint(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int number = Readint ($"Введите число N до которого нужно отбразить кубы чисел N =   ");
+int stepen = 3;
+
+
+for (int i = 1; i <= number;number--)
+{
+   Console.WriteLine($"Заданное число возведенное в куб {number}: {number}^{stepen} = " + Math.Pow(number, stepen));
+}
