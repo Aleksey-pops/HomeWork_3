2f6000435cd287425a4f4165d50225add0c06694
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-11 21:10:06 +0300

    Completion of the fourth task.

--- HomeWork_3_4/Program.cs
+++ HomeWork_3_4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Показать кубы чисел, заканчивающихся на четную цифру
+
+int Readint(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int number = Readint ($"Введите число N до которого нужно отобразить кубы чисел N = ");
+for (int i = 0; i < number;number--)
+    {
+        double num2 = Math.Pow(number,3);
+        if(num2 % 2 != 0)
+        System.Console.WriteLine($"Число заканчивается на нечетную цифру = {number} \t{num2}");  
+    }
+ 
+
+
