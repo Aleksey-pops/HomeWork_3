645eabc27c483e3bf6c01bec59dfdf5bfd1f9a17
Aleksey-pops <122688170+Aleksey-pops@users.noreply.github.com>
2023-02-11 17:47:42 +0300

    Completion of the third task.

--- HomeWork_3_3/Program.cs
+++ HomeWork_3_3/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int WorkNumber(string number)
+{
+    Console.Write(number);
+    return int.Parse(Console.ReadLine()!);
+}
+int number = WorkNumber ("Введите число N = ");
+for (int i = 1;i <= number;i++)
+    Console.WriteLine($"Произведение чисел от одного до N: {i} \t{i * i}");
+
